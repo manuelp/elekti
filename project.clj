d777a240849384273b94333679825253bd1554f3
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-06-06 22:02:32 +0200

    Updated project.clj and .gitignore

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
-(defproject filtering "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject filtering "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :description "Filter particular database tables with multiple \"orthogonal\" criterias in AND"
+  :url "https://github.com/manuelp/elekti"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-swank "1.4.4"]
+            [lein-marginalia "0.7.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [joda-time/joda-time "2.1"]])
